b749dbcca1ab0f977e54bcd145012c146772b9b3
Pablo Avila <65506165+xPabloAvilax@users.noreply.github.com>
2022-05-27 08:35:30 -0300

    Delete proyectoNuevo directory

--- proyectoNuevo/SistemaAutenticacion.js
+++
@@ -1,9 +0,0 @@
-export class SistemaAutenticacion{
-    static login(usuario,clave){
-        if("autenticable" in usuario)
-         return usuario.autenticable(clave);
-         else
-         return false;
-        
-    }
-}
